4d142faf95f0582bde4b0103d0b1ef1d9eeb19be
Charles Langlois <charles.langlois@wazo.io>
2024-10-31 15:14:38 -0400

    fixup! added bus event handler for chat messages

--- emergency_check/calld/bus_consume.py
+++ emergency_check/calld/bus_consume.py
@@ -48,13 +48,17 @@ class EventHandler:
         
         if event['user_uuid'] in emergency_check.targeted_users:
             message_content = event['content']
+            logger.info('User %s responded', event['user_uuid'])
             emergency_check.targeted_users[event['user_uuid']] = 'reached'
             if SAFE_CHAT_RESPONSE.match(message_content):
+                logger.info('User %s confirmed safe', event['user_uuid'])
                 emergency_check.targeted_users[event['user_uuid']] = 'safe'
             elif UNSAFE_CHAT_RESPONSE.match(message_content):
                 emergency_check.targeted_users[event['user_uuid']] = 'unsafe'
-        
-        logger.info('message from non-targeted user %s', event['user_uuid'])
+                logger.info('User %s confirmed unsafe', event['user_uuid'])
+        else:
+            logger.info('message from non-targeted user %s', event['user_uuid'])
 
         if all(user_state in {'safe'} for user_state in emergency_check.targeted_users.values()):
+            logger.info('All users confirmed safe, emergency check concluded')
             emergency_check.status = 'concluded'
